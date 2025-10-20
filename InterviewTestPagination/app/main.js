2572eb15cb6499707e75844815814b69386b483f
Chrystian Oliveira <chrystianmauricio98@gmail.com>
2025-10-19 21:43:25 -0300

    Adicionar arquivos de projeto.

---
+++ InterviewTestPagination/app/main.js
@@ -0,0 +1,71 @@
+﻿(function (angular) {
+    "use strict";
+
+    angular
+        .module("todoApp")
+        .directive("todoPaginatedList", [todoPaginatedList])
+        .directive("pagination", [pagination]);
+
+    /**
+     * Directive definition function of 'todoPaginatedList'.
+     * 
+     * TODO: correctly parametrize scope (inherited? isolated? which properties?)
+     * TODO: create appropriate functions (link? controller?) and scope bindings
+     * TODO: make appropriate general directive configuration (support transclusion? replace content? EAC?)
+     * 
+     * @returns {} directive definition object
+     */
+    function todoPaginatedList() {
+        var directive = {
+            restrict: "E", 
+            templateUrl: "app/templates/todo.list.paginated.html",
+            scope: {}, 
+            controller: ["$scope", "$http", controller]
+        };
+
+        function controller($scope, $http) { 
+
+            // Inicializando variáveis de escopo
+
+            $scope.todos = [];
+            $scope.loading = true;
+
+            // Chamando os dados da API
+            $http.get("api/Todo").then(function (response) { $scope.todos = response.data; }).finally(function () { $scope.loading = false });
+        }
+
+        
+
+        function link(scope, element, attrs) { }
+
+        return directive;
+    }
+
+    /**
+     * Directive definition function of 'pagination' directive.
+     * 
+     * TODO: make it a reusable component (i.e. usable by any list of objects not just the Models.Todo model)
+     * TODO: correctly parametrize scope (inherited? isolated? which properties?)
+     * TODO: create appropriate functions (link? controller?) and scope bindings
+     * TODO: make appropriate general directive configuration (support transclusion? replace content? EAC?)
+     * 
+     * @returns {} directive definition object
+     */
+    function pagination() {
+        var directive = {
+            restrict: "E", // example setup as an element only
+            templateUrl: "app/templates/pagination.html",
+            scope: {}, // example empty isolate scope
+            controller: ["$scope", controller],
+            link: link
+        };
+
+        function controller($scope) { }
+
+        function link(scope, element, attrs) { }
+
+        return directive;
+    }
+
+})(angular);
+
